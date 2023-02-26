ebaa1c889de47611068fbd896af0b5d2156db282
Roman Flores <64142185+romflorod@users.noreply.github.com>
2023-02-26 20:19:11 +0100

    setup

---
+++ bart/src/test/java/com/bart/bart/BartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bart.bart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

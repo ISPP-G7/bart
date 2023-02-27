e558d5050950e160f8bb6fc19ab13e9cb4743037
Roman Flores <64142185+romflorod@users.noreply.github.com>
2023-02-27 16:40:38 +0100

    setup

---
+++ src/main/java/com/appbart/ispp/IsppApplication.java
@@ -0,0 +1,13 @@
+package com.appbart.ispp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/appbart/ispp/IsppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appbart.ispp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

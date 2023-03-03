f06657d2a23c02637374f0af6d25fb1a29496434
mannavsic <114674003+mannavsic@users.noreply.github.com>
2023-03-03 13:35:26 +0000

    Creacion bd

---
+++ src/main/resources/db/hsqldb/data.sql
@@ -0,0 +1,5 @@
+-- Creacion manual de artistas
+INSERT INTO artistas(id, nombreUsuario, contrasena, nombreArtistico) VALUES (1, 'artista1', '4rt1st4', 'Lennon');
+
+-- Creacion manual de arrendadores
+INSERT INTO arrendadores(id, nombreUsuario, contrasena, nombreEmpresa) VALUES (1, 'empresa1', '3mpr3s4', 'Alfonso');

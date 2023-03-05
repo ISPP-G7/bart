5b58db7211982ec5515d6616a79ce094f9af277c
romflorod <romanflodriguez13@gmail.com>
2023-03-05 19:19:55 +0100

    sql

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -1,4 +1,17 @@
 -- Creacion manual de artistas
+CREATE TABLE artistas (
+    id INTEGER PRIMARY KEY,
+    nombreUsuario VARCHAR(255),
+    contrasena VARCHAR(255),
+    nombreArtistico VARCHAR(255)
+);
+CREATE TABLE arrendadores (
+    id INTEGER PRIMARY KEY,
+    nombreUsuario VARCHAR(255),
+    contrasena VARCHAR(255),
+    nombreEmpresa VARCHAR(255)
+);
+
 INSERT INTO artistas(id, nombreUsuario, contrasena, nombreArtistico) VALUES (1, 'artista1', '4rt1st4', 'Lennon');
 
 -- Creacion manual de arrendadores

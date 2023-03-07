6f26ea0afcac875d07c58320c98aff01ca699ec5
romflorod <romanflodriguez13@gmail.com>
2023-03-07 14:45:13 +0100

    crud artista con herencias a user

---
+++ SpringBootPostgresCRUDApp-main/src/main/java/com/example/SpringBootPostgresCRUD/entity/Artista.java
@@ -0,0 +1,36 @@
+package com.example.SpringBootPostgresCRUD.entity;
+
+import javax.persistence.*;
+@DiscriminatorValue("Artista")  
+@Entity
+@Table(name="artists")
+public class Artista extends User {
+
+    private String nombre_artistico;
+    private String categoria_artistica;
+
+    public Artista() {
+    }
+
+    public Artista(Long id, String firstName, String lastName, String email, String dob, String gender, String nombre_artistico, String categoria_artistica) {
+        super(id, firstName, lastName, email, dob, gender);
+        this.nombre_artistico = nombre_artistico;
+        this.categoria_artistica = categoria_artistica;
+    }
+
+    public String getNombre_artistico() {
+        return nombre_artistico;
+    }
+
+    public void setNombre_artistico(String nombre_artistico) {
+        this.nombre_artistico = nombre_artistico;
+    }
+
+    public String getCategoria_artistica() {
+        return categoria_artistica;
+    }
+
+    public void setCategoria_artistica(String categoria_artistica) {
+        this.categoria_artistica = categoria_artistica;
+    }
+}

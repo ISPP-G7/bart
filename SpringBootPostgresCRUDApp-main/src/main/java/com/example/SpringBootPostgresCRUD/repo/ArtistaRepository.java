48b59ece4c6fca34654a9f09260d13a684adff33
Miguel Ybarra <miguel.ybarra.mym@gmail.com>
2023-03-11 18:52:10 +0100

    Addanuncioartista coge el usuario logueado

--- SpringBootPostgresCRUDApp-main/src/main/java/com/example/SpringBootPostgresCRUD/repo/ArtistaRepository.java
+++ SpringBootPostgresCRUDApp-main/src/main/java/com/example/SpringBootPostgresCRUD/repo/ArtistaRepository.java
@@ -2,8 +2,11 @@ package com.example.SpringBootPostgresCRUD.repo;
 
 import com.example.SpringBootPostgresCRUD.entity.Artista;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ArtistaRepository extends JpaRepository<Artista, Long> {
+    @Query("SELECT a FROM Artista a WHERE a.email=?1")
+    Artista getArtistaByMailArtista(String mail);
 }

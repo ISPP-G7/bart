35b454a12c1556b24afb5702c76dd06e47b76352
romflorod <romanflodriguez13@gmail.com>
2023-03-13 00:15:48 +0100

    new structure

--- target/classes/db/hsqldb/data.sql
+++ target/classes/db/hsqldb/data.sql
@@ -1,20 +1,31 @@
 -- Eliminar tablas si existen
+DROP TABLE IF EXISTS message;
+DROP TABLE IF EXISTS anuncios;
+DROP TABLE IF EXISTS anuncios_artista;
+DROP TABLE IF EXISTS anuncios_arrendador;
 DROP TABLE IF EXISTS artistas;
 DROP TABLE IF EXISTS arrendadores;
-DROP TABLE IF EXISTS anuncios;
 
 -- Creación de tablas
 CREATE TABLE artistas (
     id INTEGER PRIMARY KEY,
-    nombreUsuario VARCHAR(255),
-    contrasena VARCHAR(255),
-    nombreArtistico VARCHAR(255)
+    firstName VARCHAR(255),
+    lastName VARCHAR(255),
+    email VARCHAR(255),
+    dob VARCHAR(255),
+    gender VARCHAR(255),
+    nombreArtistico VARCHAR(255),
+    categoriaArtistica VARCHAR(255)
 );
 CREATE TABLE arrendadores (
     id INTEGER PRIMARY KEY,
-    nombreUsuario VARCHAR(255),
-    contrasena VARCHAR(255),
-    nombreEmpresa VARCHAR(255)
+    firstName VARCHAR(255),
+    lastName VARCHAR(255),
+    email VARCHAR(255),
+    dob VARCHAR(255),
+    gender VARCHAR(255),
+    nombreLocal VARCHAR(255),
+    direccion VARCHAR(255)
 );
 
 CREATE TABLE anuncios (
@@ -33,9 +44,13 @@ CREATE TABLE anuncios (
 
 
 -- Inserción de datos
-INSERT INTO artistas(id, nombreUsuario, contrasena, nombreArtistico) VALUES (1, 'artista1', '4rt1st4', 'Lennon');
-INSERT INTO arrendadores(id, nombreUsuario, contrasena, nombreEmpresa) VALUES (1, 'empresa1', '3mpr3s4', 'Alfonso');
+--INSERT INTO users(firstName, lastName, email, dob, gender, password) VALUES
+--(1, 'Antonio', 'Perez', 'antonio@gmail.com', '11/05/1970', 'Male', 'password');
+INSERT INTO artistas(id, firstName, lastName, email, dob, gender, nombreArtistico, categoriaArtistica) VALUES
+(1, 'Antonio', 'Perez', 'antonio@gmail.com', '11/05/1970', 'Male', 'Paco Sans', 'Rock');
+INSERT INTO arrendadores(id, firstName, lastName, email, dob, gender, nombreLocal, direccion) VALUES
+(1, 'Paco', 'Perez', 'paco@gmail.com', '11/05/1970', 'Male', 'Bar Paco', 'Renia Mercedes N3');
 INSERT INTO anuncios(id,titulo, ubicacion, precio, especificaciones, estilo, artista_id) VALUES 
-(1,'Título del anuncio', 'Ciudad, País', 1000.00, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 'Rock', 1);
+(1,'Título del anuncio', 'Ciudad, País', 1000.00, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 'ROCK',  1);
 INSERT INTO anuncios(id,titulo, ubicacion, precio, especificaciones, estilo, arrendador_id) VALUES 
-(2,'Título del anuncio', 'Ciudad, País', 1000.00, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 'RAP', 1);
+(2,'Título del anuncio', 'Ciudad, País', 1000.00, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 'RAP',  1);

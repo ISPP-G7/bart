948eeec21506733bcb20bf6ddfa3d822f92b469a
Alejandro Ortiz Pagador <48619510+aleortpag@users.noreply.github.com>
2023-03-22 20:23:58 +0100

    Service ya funciona

--- src/main/java/com/example/SpringBootPostgresCRUD/service/TransaccionService.java
+++ src/main/java/com/example/SpringBootPostgresCRUD/service/TransaccionService.java
@@ -3,7 +3,6 @@ package com.example.SpringBootPostgresCRUD.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import com.*;
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.PaymentIntent;

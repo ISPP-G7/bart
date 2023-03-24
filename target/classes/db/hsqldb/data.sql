e4bee2be2079974368644e066563b46392217cfe
romflorod <romanflodriguez13@gmail.com>
2023-03-24 02:45:54 +0100

    arreglado problema ids sql

--- target/classes/db/hsqldb/data.sql
+++ target/classes/db/hsqldb/data.sql
@@ -16,6 +16,6 @@ INSERT INTO artistas (id,nombre_artistico, categoria_artistica,url_imagen) VALUE
 INSERT INTO	anuncios_artista (id,pseudonimo_artista,ubicacion,precio,descripcion_artista, requiere_microfono,requiere_instrumentos,requiere_iluminacion,requiere_altavoces,requiere_mesa_de_mezclas, requiere_portatil, requiere_otras_especificaciones,ofrece_actuacion_por_entradas,estilo,artista_id,esta_aceptado) VALUES
 	(1,'platonico','Sevilla',300,'Toco rock fusión con tango',false,false,false,false,false,false,'', false,'ROCK',2,false);
 INSERT INTO	anuncios_arrendador (id,nombre_local,ubicacion,precio,descripcion_arrendador, estilo,ofrece_microfono,ofrece_instrumentos,ofrece_iluminacion,ofrece_altavoces, ofrece_mesa_de_mezclas,ofrece_portatil,ofrece_otras_especificaciones,ofrece_actuacion_por_entradas,arrendador_id,esta_aceptado) VALUES
-	(1,'Bocadillos Ñam Ñam','Avenida Reina Mercedes,31',100,'A nuestro público les gusta el rock','ROCK',' ',' ',' ',' ',' ',' ',' ',true,1,false);
+	(4,'Bocadillos Ñam Ñam','Avenida Reina Mercedes,31',100,'A nuestro público les gusta el rock','ROCK',' ',' ',' ',' ',' ',' ',' ',true,1,false);
 INSERT INTO messages (id,message_body,date,user_sender,user_receiver) VALUES
 	(1,'Hola, estarías dispuesto a tocar en mi garito?','2023-03-22',1,2);

24d83dc2e20c95ca671c16283a73120a5cc3336a
Roman flores <romanflodriguez13@gmail.com>
2023-03-28 16:16:25 +0200

    jar

--- target/classes/db/hsqldb/data.sql
+++ target/classes/db/hsqldb/data.sql
@@ -7,7 +7,7 @@ TRUNCATE TABLE users CASCADE;
 TRUNCATE TABLE fotos CASCADE;
 
 -- Creación de tablas
-INSERT INTO users (id,first_name,last_name,email,dob,gender,password,es_arrendador,es_artista) VALUES
+/*INSERT INTO users (id,first_name,last_name,email,dob,gender,password,es_arrendador,es_artista) VALUES
 	(1,'José','Fernández', 'namnam@gmail.com','venta de bocadillos y dulce en reina mercedes', 'Masculino','joseNamNam',true,false),
 	(2,'Manolo', 'Platero', 'platero@gmail.com','El mejor rock de los 80 hasta ahora', 'Masculino', 'platero', false, true);
 INSERT INTO arrendadores (id,direccion,nombre_local,url_imagen) VALUES
@@ -19,4 +19,4 @@ INSERT INTO	anuncios_artista (id,pseudonimo_artista,ubicacion,precio,descripcion
 INSERT INTO	anuncios_arrendador (id,nombre_local,ubicacion,precio,descripcion_arrendador, estilo,ofrece_microfono,ofrece_instrumentos,ofrece_iluminacion,ofrece_altavoces, ofrece_mesa_de_mezclas,ofrece_portatil,ofrece_otras_especificaciones,ofrece_actuacion_por_entradas,arrendador_id,esta_aceptado,esta_pagado) VALUES
 	(4,'Bocadillos Ñam Ñam','Avenida Reina Mercedes,31',100,'A nuestro público les gusta el rock','ROCK',' ',' ',' ',' ',' ',' ',' ',true,1,false,false);
 INSERT INTO messages (id,message_body,date,user_sender,user_receiver) VALUES
-	(5,'Hola, estarías dispuesto a tocar en mi garito?','2023-03-22',1,2);
+	(5,'Hola, estarías dispuesto a tocar en mi garito?','2023-03-22',1,2);*/

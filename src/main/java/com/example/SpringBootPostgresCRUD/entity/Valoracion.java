1c1d2266c87771636e40f47a00d98bc45c7caca2
alvrodgar2 <alvrodgar2@alum.us.es>
2023-04-06 21:02:22 +0200

    Clase valoración incompleta

---
+++ src/main/java/com/example/SpringBootPostgresCRUD/entity/Valoracion.java
@@ -0,0 +1,42 @@
+package com.example.SpringBootPostgresCRUD.entity;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "valoraciones")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Valoracion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "sender")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name = "reciever")
+    private User receiver;
+    
+    private Integer nota;
+    private String mensaje;
+    private Date fecha;
+    
+}
